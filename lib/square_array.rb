e1d79d860c3c0f4637cd0814fc0a251b90acefdd
Veronica Lopez <nicaa0695@gmail.com>
2019-07-03 22:01:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,7 @@
-def square_array(array)
-  a = 1
-  while a >= 8
-  puts a 
-  a += 1 
-end
-  arr = [1, 2, 3, 4]
-  square_array(arr)
-  new_array = [5, 6, 7, 8]
-  square_array(new_array)
-end
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
+ end
