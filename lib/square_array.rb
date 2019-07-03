53f6f578f2ab58d592ecfd8ca8c5cf8a5c3a3267
Veronica Lopez <nicaa0695@gmail.com>
2019-07-03 22:10:46 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  arr = [1, 2, 3, 4]
+  ([1, 2, 3]).each(no args)
+  square_array(arr)
 end
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |num|
+    new_numbers << (num ** 2),
+  end
+  return new_numbers
+ end
